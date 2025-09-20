813fc52ebb0317cd2c85e0abc9b184e5249264a9
AdrianVincent17 <adrian.vicente.lopez@ces-vegamedia.es>
2025-09-20 15:49:24 +0200

    ejercicio 5

---
+++ Ejercicios/Ejercicio 5.php
@@ -0,0 +1,18 @@
+<?php
+
+define("PI", 3.1416);
+define("LIMITE", 365);
+define("NOMBRE", "Juan Perez");
+define("EDAD", 30); 
+
+echo "El valor de PI es: " . PI;
+echo <br>;
+echo "El limite es: " . LIMITE;
+echo <br>;
+echo "El nombre es: " . NOMBRE;
+echo <br>;
+echo "La edad es: " . EDAD;
+
+
+
+?>
